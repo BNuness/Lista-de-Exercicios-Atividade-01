51bda9db7b52c1844f67fc2017e4ac8ec24571fb
BNuness <88894723+BNuness@users.noreply.github.com>
2021-08-23 09:36:29 -0300

    Delete Ex 4 - Lista comp.py

--- Ex 4 - Lista comp.py
+++
@@ -1,7 +0,0 @@
-cpf = input("CPF(xxx.xxx.xxx-xx) :")
-
-for letra in cpf:
-    if(cpf[3] !=".") or (cpf[7] !=".") or (cpf[11] !="-"):
-        cpf = input("O 'CPF' pricisa estar no formato (xxx.xxx.xxx-xx) :")
-    else:
-        print("O 'CPF' está no formato correto")
