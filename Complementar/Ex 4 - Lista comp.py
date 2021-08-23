fc20e45a13bca7909391fa2368669233a39215c1
BNuness <88894723+BNuness@users.noreply.github.com>
2021-08-23 13:01:14 -0300

    Add files via upload

---
+++ Complementar/Ex 4 - Lista comp.py
@@ -0,0 +1,7 @@
+cpf = input("CPF(xxx.xxx.xxx-xx) :")
+
+
+while (cpf[3] !=".") or (cpf[7] !=".") or (cpf[11] !="-"):
+        cpf = input("O 'CPF' pricisa estar no formato (xxx.xxx.xxx-xx) :")
+else:
+        print("O 'CPF' está no formato correto")
