0c6f08817f2e0d727dda68cb0c010cf88ab4347e
BNuness <88894723+BNuness@users.noreply.github.com>
2021-08-23 09:35:04 -0300

    Delete Ex 2 - Lista comp.py

--- Ex 2 - Lista comp.py
+++
@@ -1,6 +0,0 @@
-def somaImposto(taxaImposto, Custo):
-    return (1 + taxaImposto/100)*Custo
-t = float(input('Entre com a taxa de imposto: '))
-c = float(input('Entre com o custo: '))
-somaImposto = round(somaImposto(t,c),2)
-print('Valor com imposto: ', somaImposto)
